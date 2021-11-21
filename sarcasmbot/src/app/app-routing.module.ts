93128e19f988d2514750d0e3ee78f1439ec49623
Chandan Gowda <chandangowda@Chandans-MacBook-Pro.local>
2021-11-20 21:49:58 -0700

    home screen

--- sarcasmbot/src/app/app-routing.module.ts
+++ sarcasmbot/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DocsComponent } from './docs/docs.component';
+import { ChatbotComponent } from './home/chatbot/chatbot.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: "full" },
+  { path: 'home', component: HomeComponent, children: [{ path: "chatbot", component: ChatbotComponent }] },
+  { path: 'docs', component: DocsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- sarcasmbot/src/app/app.module.ts
+++ sarcasmbot/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DocsComponent } from './docs/docs.component';
+import { ChatbotComponent } from './home/chatbot/chatbot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DocsComponent,
+    ChatbotComponent
   ],
   imports: [
     BrowserModule,
