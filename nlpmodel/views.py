4e012a9d20b44ec515ebbb21e06d539698840c76
Chandan Gowda <chandangowda@Chandans-MacBook-Pro.local>
2021-12-01 11:11:35 -0700

    nlp model integration

--- nlpmodel/views.py
+++ nlpmodel/views.py
@@ -3,7 +3,12 @@ from django.http.response import HttpResponseNotFound, JsonResponse
 from django.http import HttpResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
+from nlp_2 import *
+import joblib
 
 @csrf_exempt
 def SarcasmBotReply(request):
+    model = joblib.load('./chatbotModel.sav')
+    # botreply = model.userinput('Hi')
+    # print('bot', botreply)
     return JsonResponse({'result':'Hello World'})
